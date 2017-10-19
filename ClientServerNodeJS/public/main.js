f1bd46e732b065b4e6177e49b105cb9f6fe9b2ed
zz871997 <zz15121998@gmail.com>
2017-10-19 20:58:50 +0700

    Revert "Delete Network"
    This reverts commit d23678b38c75ee8a076f20573ffa820128eaef24.

--- ClientServerNodeJS/public/main.js
+++ ClientServerNodeJS/public/main.js
@@ -1,92 +1,73 @@
 /**
  * Created by Tran Quang Khai on 10/6/2017.
  */
-$(function () {
-    var FADE_TIME = 150;    //ms
-    var TYPING_TIME_LENGTH = 400;
-    var CORLOR = [
-        '#e21400', '#91580f', '#f8a700', '#f78b00',
-        '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
-        '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
-    ];
+var socket = io("http://localhost:3000");
 
-    //Initialize variables
-    var $window = $(window);
-    var $usernameInput = $('#txtUsername');   //Input for username
-    var $messages = $('#listMessages');       //Message Area
-    var $inputMessage = $('#inputMessages');    //Input message input box
+// Register - failed
+socket.on("server-send-register-fail", function () {
+    alert("Tên username đã tồn tại");
+});
 
-    var $loginPage = $('#loginForm');
-    var $chatPage = $('#chatForm');
+// Register - success
+socket.on("server-send-register-success", function (data) {
+    $("#currentUser").html(data);
+    $("#loginForm").hide(1000);
+    $("#chatForm").show(2000);
+});
 
-    //Prompt for setting a username
-    var username;
-    var connected = false;
-    var typing = false;
-    var lastTypingTime;
-    var $currentInput = $usernameInput.focus();
+// Client listens event "Server- send-list-users"
+socket.on("server-send-list-users", function (data) {
+    $("#boxContent").html("");
+    data.forEach(function (i) {
+        $("#boxContent").append("<div class='user'>" + i + "</div>");
+    });
 
-    var socket = io();
+});
 
-    // function addParticipantsMessage(data) {
-    //     var message = '';
-    //     if (data.numUsers === 1) {
-    //         message += "There's 1 participant";
-    //     }
-    //     else {
-    //         message += "There are " + data.numUsers + "participants";
-    //     }
-    //     log(message);
-    // }
-    //
-    // // Set the client's username
-    // function setUsername() {
-    //     ////////////////////////////////////////////////////
-    //     username =  cleanInput($usernameInput.val().trim());
-    //     /////////////////////////////////////////////////////
-    //
-    //     //If the username is valid
-    //     if (username) {
-    //         $loginPage.fadeOut();
-    //         $chatPage.show();
-    //         $loginPage.off('click');    // Remove an event handler
-    //         $currentInput = $inputMessage.focus();
-    //
-    //         //Tell the server your username
-    //         socket.emit('add user', username);
-    //     }
-    // }
-    //
-    // // Sends a chat message
-    // function sendMessage() {
-    //     var message = $inputMessage.val();
-    //     // Prevent markup from being injected into the message
-    //     message = clearInput(message);
-    //     /////////////////////////////////////////////////
-    //
-    //     // if there is a non-empty message and a socket connection
-    //     if(message && connected) {
-    //         $inputMessage.val('');
-    //         //Add Chat Message - add a JSON
-    //         addChatMessage({
-    //             username: username,
-    //             message: message
-    //         });
-    //         //Tell server to execute 'new message' and send along one parameter
-    //         socket.emit('new message', message);
-    //     }
-    // }
-    //
-    // // Log a message
-    // function log(message, options) {
-    //
-    //
-    //
-    // }
-    //
+// Listen server send message
+socket.on("server-send-message", function (data) {
+    $("#listMessages").append("<div class='message'>" + data.username+ " :" + data.mess);
+});
 
+// Listen event who is typing
+// socket.on("server-send-who-is-typing", function (data) {
+//     $("#status").html(data);
+// });
+//
+// // Listen event "Stop Typing"
+// socket.on("server-send-who-is-typing", function () {
+//     $("#status").html("*******************");
+// });
 
+$(document).ready(function () {
+    $("#loginForm").show();
+    $("#chatForm").hide();
 
+    $("#btnRegister").click(function () {
+        // Emit username to server (node)
+        socket.emit("client-send-username", $("#txtUsername").val());
+    });
 
+    $("#btnLogout").click(function () {
+        $("#txtMessages").val("");
+        $("#listMessages").val("");
+        // Emit to server: I want to logout
+        socket.emit("i-want-to-logout");
+        $("#chatForm").hide(1000);
+        $("#loginForm").show(1000);
 
+});
+
+    $("#btnSendMessage").click(function (data) {
+        socket.emit("user-send-message", $("#txtMessages").val());
+        $("#txtMessages").val('');
+    });
+
+    // $("#txtMessages").focusin(function () {
+    //     socket.emit("i-am-typing");
+    // });
+    //
+    // $("#txtMessages").focusout(function () {
+    //     socket.emit("stop-typing");
+    // });
 });
